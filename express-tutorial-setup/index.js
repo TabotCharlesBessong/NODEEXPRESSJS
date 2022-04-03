312cd4782b8e4aa1feb8383f36b9c23fe4f21374
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-03 18:07:26 +0100

    query parameters

--- express-tutorial-setup/index.js
+++ express-tutorial-setup/index.js
@@ -8,6 +8,12 @@ app.get('/',(req,res)=>{
   res.send('<h1>Home page is here</h1><a href="/api/products">products</a>')
 })
 
+const len = products.length
+console.log(len);
+
+const ran = Math.floor(Math.random() * len) 
+console.log(ran);
+
 app.get('/api/products',(req,res)=>{
   const newProducts = products.map((prop)=>{
     const {id,name,image} = prop
@@ -24,6 +30,32 @@ app.get('/api/products/:productID',(req,res)=>{
     return res.status(404).send('Product does not exist in our store')
   }
   res.json(singleProducts)
+})
+
+app.get('/api/v1/query',(req,res)=>{
+  console.log(req.query);
+  const {search,limit} = req.query
+  let sortedProd = [...products]
+  if(search){
+    sortedProd = sortedProd.filter((prod)=>{
+      return prod.name.startsWith(search)
+    })
+  }
+
+  let norm = limit > len ? ran : limit
+
+  if(norm){
+    sortedProd = sortedProd.slice(0,Number(limit))
+  }
+
+  if(sortedProd.length < 1){
+    // res.status(200).send('No products found')
+    return res.status(200).json({success:true,data:[]})
+  }
+
+  res.status(200).json(sortedProd)
+  res.send("Hello world")  
+  
 })
 
 app.listen(port,()=>{
