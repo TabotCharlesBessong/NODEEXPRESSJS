850c096dc724c85feff58ea450e1680ebb233b13
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-03 19:17:44 +0100

    morgan packages

--- express-tutorial-setup/some/authorise.js
+++ express-tutorial-setup/some/authorise.js
@@ -1,4 +1,17 @@
 
+
+
+
+// const authorise = require('./some/authorise')
+const logger = require('./logger')
+const express = require('express')
+// const res = require('express/lib/response')
+const morgan = require('morgan')
+const port = 5000
+const app = express()
+// app.use(logger)
+// app.use([logger,authorise])
+
 const authorised = (req,res,next)=>{
   const {user} = req.query
   if(user === 'John'){
@@ -10,5 +23,27 @@ const authorised = (req,res,next)=>{
   console.log('authority')
   next()
 }
+// req => middleware => res
+app.use(morgan('tiny'))
+app.use(express.static('./public'))
 
-module.exports = authorised
+app.get('/',(req,res)=>{
+  
+  res.send('The Home')
+})
+app.get('/about',(req,res)=>{
+  res.send('This is our story')
+})
+app.get('/api/products',(req,res)=>{
+  res.send('Buy our products')
+})
+
+app.get('/api/items',(req,res)=>{
+  res.send('Buy our items')
+})
+
+app.listen(port,()=>{
+  console.log(`The server is running on port ${port}`);
+})
+
+// module.exports = authorised
