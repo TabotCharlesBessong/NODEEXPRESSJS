40ce6d7c3645089399bba75a0271ad1db4a39930
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-04 06:59:44 +0100

    removing duplicates files

--- express-tutorial-setup/index.js
+++ express-tutorial-setup/index.js
@@ -2,3 +2,20 @@ const express = require('express')
 const morgan = require('morgan')
 const port = 5000
 const app = express()
+let people = require('./data')
+
+// static assets
+app.use(express.static('./methods-public'))
+
+
+app.get('/api/people',(req,res)=>{
+  res.status(200).json({success:true,data:people})
+})
+
+app.get('/',(req,res)=>{
+  res.send('<h1>Hello world</h1>')
+})
+
+app.listen(port,()=>{
+  console.log(`The server is listening on port ${port}`)
+})
