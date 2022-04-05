097c8fa4c1b391dfc8212b81087d9ee86523afcd
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-05 20:38:05 +0100

    setting up all routes

--- 3-task-manager-setup/controllers/tasks.js
+++ 3-task-manager-setup/controllers/tasks.js
@@ -2,9 +2,29 @@
 const express = require('express')
 const getAllTasks = (req,res)=>{
   // res.rend('all items')
-  res.send('all tasks')
+  res.send('get all tasks')
+}
+
+const createTasks = (req,res)=>{
+  res.json(req.body);
+}
+
+const getTask = (req,res)=>{
+  res.json({id:req.params.id});
+}
+
+const updateTask = (req,res)=>{
+  res.send('Hello old tasks');
+}
+
+const deleteTask = (req,res)=>{
+  res.send('Bye Bye tasks');
 }
 
 module.exports = {
-  getAllTasks
+  getAllTasks,
+  updateTask,
+  createTasks,
+  getTask,
+  deleteTask
 }
--- 3-task-manager-setup/routes/tasks.js
+++ 3-task-manager-setup/routes/tasks.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const {getAllTasks} = require('../controllers/tasks')
+const {getAllTasks,createTasks,getTask,deleteTask,updateTask} = require('../controllers/tasks')
 
-router.route('/').get(getAllTasks)
+router.route('/').get(getAllTasks).post(createTasks)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
 module.exports = router
