8fd293a7fc6db32b34fe12fd7d45cb01770e478a
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-23 08:09:28 +0100

    started basic login functions

--- 6-jobs-API-starter/backend/controllers/auth.js
+++ 6-jobs-API-starter/backend/controllers/auth.js
@@ -1,5 +1,6 @@
 
 const BadRequestErr = require('../errors/bad-request')
+const UnauthenticatedErr = require('../errors/unauthenticated')
 const { json } = require('express/lib/response')
 const {StatusCodes} = require('http-status-codes')
 const User = require('../models/User')
@@ -14,7 +15,20 @@ const register = async (req,res)=>{
 }
 
 const login = async (req,res)=>{
-  res.send('login User')
+  // res.send('login User')
+  const {email,password} = req.body
+  if(!email || !password){
+    throw new BadRequestErr('Please provide email and password')
+  }
+
+  const user = await User.findOne({email}) 
+  // comparing the pasword 
+  if(!user){
+    throw new UnauthenticatedErr('Invalid cridentials')
+  }
+
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({user:{name:user.name},token})
 }
 
 module.exports = {
--- 6-jobs-API-starter/backend/models/User.js
+++ 6-jobs-API-starter/backend/models/User.js
@@ -32,7 +32,7 @@ UserSchema.pre('save',async function(next){
 })
 
 UserSchema.methods.createJWT = function (){
-  return jwt.sign({userId:this._id,name:this.name},'jwtSecret',{expiresIn:'30d'})
+  return jwt.sign({userId:this._id,name:this.name},process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME})
 }
 
 module.exports = mongoose.model('User',UserSchema)
