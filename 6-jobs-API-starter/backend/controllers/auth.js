fc3b80dc845fe65772c774cdba623e70ebdaf6bb
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-23 07:45:56 +0100

    done with the register routes

--- 6-jobs-API-starter/backend/controllers/auth.js
+++ 6-jobs-API-starter/backend/controllers/auth.js
@@ -3,18 +3,14 @@ const BadRequestErr = require('../errors/bad-request')
 const { json } = require('express/lib/response')
 const {StatusCodes} = require('http-status-codes')
 const User = require('../models/User')
-const bcrypt = require('bcryptjs')
 
 const register = async (req,res)=>{
-  // const {name,email,password} = req.body
-  // if(!name || !email || !password){
-  //    throw new BadRequestErr('Please provide the name email and password')
-  // }
-
-  // temp user object
+  
   
   const user = await User.create({...req.body})
-  res.status(StatusCodes.CREATED).json({user})
+  const token = user.createJWT()
+  
+  res.status(StatusCodes.CREATED).json({user:{name:user.name},token})
 }
 
 const login = async (req,res)=>{
--- 6-jobs-API-starter/backend/models/User.js
+++ 6-jobs-API-starter/backend/models/User.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 
 const UserSchema = new mongoose.Schema({
@@ -29,5 +30,9 @@ UserSchema.pre('save',async function(next){
   this.password = await bcrypt.hash(this.password,salt)
   next()
 })
+
+UserSchema.methods.createJWT = function (){
+  return jwt.sign({userId:this._id,name:this.name},'jwtSecret',{expiresIn:'30d'})
+}
 
 module.exports = mongoose.model('User',UserSchema)
